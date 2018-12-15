a4df3ebf8de3825663365e8a423bd1a4efe61d8a
haefner <lisa-maria.haefner@hs-bochum.de>
2018-12-15 17:23:33 +0100

    Einkommentierung

--- Praktikumsteil3/LocUtil/src/Implementierung/Main.java
+++ Praktikumsteil3/LocUtil/src/Implementierung/Main.java
@@ -8,17 +8,17 @@ import Diagramme.Diagram;
 public class Main {
 	
 	public static void main(String[] args) {
-//		Empirical_FP_NN eFPNN = new Empirical_FP_NN();
-//		eFPNN.berechneEmpiricalFP_NN();
-//		
-//		Empirical_FP_KNN eFPKNN = new Empirical_FP_KNN();
-//		eFPKNN.berechneEmpiricalFP_KNN(3);
-//		
-//		Model_FP_NN mFPNN = new Model_FP_NN();
-//		mFPNN.berechneModel_FP_NN();
-//		
-//		Model_FP_KNN mFPKNN = new Model_FP_KNN();
-//		mFPKNN.berechneModel_FP_KNN(3);
+		Empirical_FP_NN eFPNN = new Empirical_FP_NN();
+		eFPNN.berechneEmpiricalFP_NN();
+		
+		Empirical_FP_KNN eFPKNN = new Empirical_FP_KNN();
+		eFPKNN.berechneEmpiricalFP_KNN(3);
+		
+		Model_FP_NN mFPNN = new Model_FP_NN();
+		mFPNN.berechneModel_FP_NN();
+		
+		Model_FP_KNN mFPKNN = new Model_FP_KNN();
+		mFPKNN.berechneModel_FP_KNN(3);
 		
 		
 		Diagram diagram= new Diagram("testDiagram");
