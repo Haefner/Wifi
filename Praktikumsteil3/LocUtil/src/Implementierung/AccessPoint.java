8373b1c86617d037068b6183f4496ad88cc265c6
lnapppenfeld <Leon.Nappenfeld@hs-bochum.de>
2018-12-18 13:22:43 +0100

    Name des jeweiligen APs wird in das Diagramm geschrieben

--- Praktikumsteil3/LocUtil/src/Implementierung/AccessPoint.java
+++ Praktikumsteil3/LocUtil/src/Implementierung/AccessPoint.java
@@ -7,6 +7,7 @@ public class AccessPoint {
 	
 	private MACAddress address;
 	private GeoPosition position;
+	private String name;
 	
 	
 	
@@ -14,12 +15,12 @@ public class AccessPoint {
 		super();
 		this.address = MACAddress.parse(address);
 		this.position = new GeoPosition(x,y,z);
+		this.name = convertMacToName(MACAddress.parse(address));
 	}
 
 	public MACAddress getAddress() {
 		return address;
 	}
-	
 	public void setAddress(MACAddress address) {
 		this.address = address;
 	}
@@ -29,6 +30,40 @@ public class AccessPoint {
 	public void setPosition(GeoPosition position) {
 		this.position = position;
 	}
+	public String getName() {
+		return name;
+	}
 	
-	
+	public String convertMacToName(MACAddress address) {
+		String apName = "";
+
+        switch (address.toString()) {
+            case "00:14:BF:B1:7C:54": 	apName = "AP1";
+                     					break;
+            case "00:16:B6:B7:5D:8F":  	apName = "AP2";
+                    					break;
+            case "00:14:BF:B1:7C:57":  	apName = "AP3";
+										break;
+            case "00:14:BF:B1:97:8D": 	apName = "AP4";
+										break;
+			case "00:16:B6:B7:5D:9B":  	apName = "AP5";
+										break;
+			case "00:14:6C:62:CA:A4": 	apName = "AP6";
+										break;
+			case "00:14:BF:3B:C7:C6":  	apName = "AP7";
+										break;
+			case "00:14:BF:B1:97:8A": 	apName = "AP8";
+										break;
+			case "00:14:BF:B1:97:81":  	apName = "AP9";
+										break;
+			case "00:16:B6:B7:5D:8C": 	apName = "AP10";
+										break;
+			case "00:11:88:28:5E:E0":  	apName = "AP11";
+										break;
+			default:					apName = "Other AP";
+										break;
+        }
+		return apName;	
+	}
+
 }
