2158bc92aa547adcaee921ab84880ece0d5b134f
haefner <lisa-maria.haefner@hs-bochum.de>
2018-12-18 12:08:08 +0100

    Formatierung korrigiert

--- Praktikumsteil3/LocUtil/src/Diagramme/MedianKBasiertEmpirisch.java
+++ Praktikumsteil3/LocUtil/src/Diagramme/MedianKBasiertEmpirisch.java
@@ -36,23 +36,22 @@ public class MedianKBasiertEmpirisch {
 	}
 	
 	private double berechneEmpirisch(int k) {
-	List<Double> fehlerK = new ArrayList<>();
-	Score_NN score_NN = new Score_NN();
-	for(int i=0; i<10;i++) {
-	Empirical_FP_KNN eFPKNN = new Empirical_FP_KNN();
-	HashMap<GeoPosition, GeoPosition>  map= eFPKNN.berechneEmpiricalFP_KNN(k);
-	
-	List<Double> fehler= Score_NN.berechneFehler(map);
-	List<Double> sortierterFehler=score_NN.sortiereFehlerList(fehler);
-	int mitte =sortierterFehler.size()/2;
-	fehlerK.add(sortierterFehler.get(mitte));
-	}
-	List<Double> sortierterFehlerK= score_NN.sortiereFehlerList(fehlerK);
-	int mitteK =sortierterFehlerK.size()/2;
-	System.out.println("Median Fehler:"+sortierterFehlerK.get(mitteK));
-	return sortierterFehlerK.get(mitteK);
-	
-	
+		List<Double> fehlerK = new ArrayList<>();
+		Score_NN score_NN = new Score_NN();
+		for (int i = 0; i < 10; i++) {
+			Empirical_FP_KNN eFPKNN = new Empirical_FP_KNN();
+			HashMap<GeoPosition, GeoPosition> map = eFPKNN.berechneEmpiricalFP_KNN(k);
+
+			List<Double> fehler = Score_NN.berechneFehler(map);
+			List<Double> sortierterFehler = score_NN.sortiereFehlerList(fehler);
+			int mitte = sortierterFehler.size() / 2;
+			fehlerK.add(sortierterFehler.get(mitte));
+		}
+		List<Double> sortierterFehlerK = score_NN.sortiereFehlerList(fehlerK);
+		int mitteK = sortierterFehlerK.size() / 2;
+		System.out.println("Median Fehler:" + sortierterFehlerK.get(mitteK));
+		return sortierterFehlerK.get(mitteK);
+
 	}
 	
 }
